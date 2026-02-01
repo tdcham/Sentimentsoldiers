b71f68d9fcf98eb6ef266c8270d30e551614a31f
ykk9fx <112348542+ykk9fx@users.noreply.github.com>
2026-02-01 15:27:56 -0500

    added fetch for U prices and Nuclear fuel %

--- datamerge.py
+++ datamerge.py
@@ -4,4 +4,5 @@ import pandas as pd
 
 "Sentimentsoldiers/Bisconti_2025_digitized_figure.csv"
 
-
+uranium_price = pd.read_csv("SentimentSoldiers/Uranium Purchace Price - Sheet1.csv", usecols=['Year', 'U.S.-origin uranium (weighted-average price)', 'Foreign-origin uranium (weighted-average price)'])
+nuclear_fuel_share = pd.read_csv("SentimentSoldiers/US Nuclear Generating Statistics - Sheet1.csv", usecols=['Year', 'Nuclear Fuel Share (Percent)'])
