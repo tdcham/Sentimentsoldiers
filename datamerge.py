1e3f3e123ae08bf6fe55f8c9b1980d072ea6d8c5
tdcham <trinityd302@gmail.com>
2026-02-01 15:58:35 -0500

    getting changes

--- datamerge.py
+++ datamerge.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 # Using year 2000 as the reference year
 
-"Sentimentsoldiers/Bisconti_2025_digitized_figure.csv"
+df1 = pd.read_csv("Bisconti_2025_digitized_figure.csv")
+df2 = pd.read_csv("Table_8.1_Nuclear_Energy_Overview.csv")
 
+
+print(df1.head())
 
