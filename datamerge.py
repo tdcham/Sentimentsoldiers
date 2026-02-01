a27380fdcf801a050ed1f171d15bfc5a1869005f
K Freese <kfreese@MacBook-Pro-6.local>
2026-02-01 15:30:46 -0500

    updates

--- datamerge.py
+++ datamerge.py
@@ -5,3 +5,12 @@ import pandas as pd
 "Sentimentsoldiers/Bisconti_2025_digitized_figure.csv"
 
 
+
+# Gallup Data
+"Gallup_Alternative_vs_NonRenewable.csv"
+"Gallup_Availability_Afforability_Energy.csv"
+"Gallup_OpinionsonNuclear.csv"
+"Gallup_PartyIdentification.csv"
+
+"International Energy Agency - electricity generation sources in United States.csv"
+"yearly_US_only.csv"
